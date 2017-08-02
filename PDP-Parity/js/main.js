8a8beae9366f94fe7bf51513cf1d20550d92d3b4
joenewman <joenewman1977@gmail.com>
2017-08-02 13:52:47 +0200

    Add pinned 'Book Now' styling

--- PDP-Parity/js/main.js
+++ PDP-Parity/js/main.js
@@ -39,3 +39,12 @@ $(".urgency_question").click(function () {
         $(".urgency").hide();
     }
 });
+$(".pin").hide();
+$(".pin_question").click(function () {
+    if ($(this).is(":checked")) {
+        $(".pin").show();
+    }
+    else {
+        $(".pin").hide();
+    }
+});
